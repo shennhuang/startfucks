d0c14cf360719ed7020cee8901d7ad6abcb4c14a
shen.huang <shen.huang@shenhuangde-MacBook-Pro.local>
2017-07-19 23:54:28 +0800

    fix confilcti

--- route.js
+++ route.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express();
 
-
 var router = express.Router();
 
 var home = require('./routes/home');
 var start = require('./routes/start');
-var login = require('./routes/login')
-var signup = require('./routes/signup')
+ var apis = require('./routes/apis');
 
 router.get('/', function(req, res) {
     res.redirect('start');
@@ -22,5 +20,7 @@ router.use('/start', function(req, res) {
     start(req, res);
 });
 
-
+router.post('/apis', function(req, res){
+      apis(req, res);
+});
 module.exports = router;
