b16aaf052d978a50f2c316f1247f22cacaa229df
Comi <comi.hu@comihu-MBP.local>
2017-07-20 18:42:34 +0800

    修一下登入後資料顯示問題

--- routes/home.js
+++ routes/home.js
@@ -6,29 +6,29 @@ function home(req, res) {
     }
 
     let account = req.session.account;
+
+    var result = {
+
+    [account]: {
+            name: account,
+            settings: [
+            {
+                title: "Time",
+                subtitle: "Taiwan",
+                gridItemSize: {width: 1, height: 1},
+                gridItemLocation: {x: 0, y: 0},
+            },
+            {
+                title: "Weather",
+                subtitle: "New Taipei City",
+                gridItemSize: {width: 2, height: 1},
+                gridItemLocation: {x: 0, y: 0},
+            },]     
+        }
+
+    };
+
     return res.render('home', {result: result[account], gridRowNum:10});
 }
 
 module.exports = home;
-
-// test data
-var result = {
-
-    'comi': {
-        name: "User",
-        settings: [
-        {
-            title: "Time",
-            subtitle: "Taiwan",
-            gridItemSize: {width: 1, height: 1},
-            gridItemLocation: {x: 0, y: 0},
-        },
-        {
-            title: "Weather",
-            subtitle: "New Taipei City",
-            gridItemSize: {width: 2, height: 1},
-            gridItemLocation: {x: 0, y: 0},
-        },]     
-    }
-
-};
