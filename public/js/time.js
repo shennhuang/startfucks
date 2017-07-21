0e8da3d5c3ce360dd0e82b0253a09fefa2497e3a
Comi <comi.hu@comihu-MBP.local>
2017-07-21 09:26:31 +0800

    改一些前端設定而已

--- public/js/time.js
+++ public/js/time.js
@@ -2,4 +2,5 @@ function time(){
     document.getElementById('Time_Taiwan').children[1].innerHTML = new Date().toLocaleString();
     setTimeout('time()',1000);
 }
+document.getElementById('Time_Taiwan').children[1].setAttribute("style","font-size:20px");
 time();
