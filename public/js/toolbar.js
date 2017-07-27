2bc436f7934406c32d15f7243c1e09c460defe15
shen.huang <shen.huang@104.com.tw>
2017-07-27 11:16:20 +0800

    修理小bug

--- public/js/toolbar.js
+++ public/js/toolbar.js
@@ -18,6 +18,16 @@ function hiddenBlockList(){
     document.getElementsByClassName("blockList")[0].style.display = "none";    
     document.getElementById('blockListBtn').style.opacity = 0.5;
 }
+
+function blockListBtnOnmouseover(x){
+    if(document.getElementsByClassName("blockList")[0].style.display == "none" )
+        x.style.opacity = 1;
+}
+
+function blockListBtnOnmouseout(x){
+    if(document.getElementsByClassName("blockList")[0].style.display == "none" )
+        x.style.opacity = 0.5;
+}
 // function listBtnOnMouseOver(){
 //     document.getElementsByClassName("blockList")[0].style.display = "block";
 // }
@@ -40,4 +50,14 @@ function settingListBtnOnclick(){
 function hiddenSettingList(){
     document.getElementsByClassName("settingList")[0].style.display = "none";    
     document.getElementById('settingListBtn').style.opacity = 0.5;
+}
+
+function settingListBtnOnmouseover(x){
+    if(document.getElementsByClassName("settingList")[0].style.display == "none" )
+        x.style.opacity = 1;
+}
+
+function settingListBtnOnmouseout(x){
+    if(document.getElementsByClassName("settingList")[0].style.display == "none" )
+        x.style.opacity = 0.5;
 }
