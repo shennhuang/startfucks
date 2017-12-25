f10b30d7577276404616de7c0f4bb379b631605d
LittleQPig <littleqpig@LittleQPigde-MacBook-Pro.local>
2017-12-25 18:12:35 +0800

    緊急修正AQI錯誤

--- apis/getcwbAQI.js
+++ apis/getcwbAQI.js
@@ -5,17 +5,21 @@ let apidata = require('../public/data/apidata.json');
 function getcwbAQI(req, res){
 
     let stationName = req.body.areaAndStationName.split("_")[1];
-    let url = "https://taqm.epa.gov.tw/taqm/aqs.ashx?lang=tw&act=aqi-epa"
+    let url = "http://taqm.epa.gov.tw/taqm/aqs.ashx?lang=tw&act=aqi-epa"
     let options = { 
         method: 'GET',
         url,
         gzip: true,
         json: true,
+        headers: {
+            
+        },
     };
-
     request(options, function (error, response, body) {
-        if (error) console.log(error);
-    
+        if (error) {
+            console.log(error);
+            return res.send('');
+        }
         if(body.Result === "ok") {
             let data = body.Data;
             for(i = 0 ; i < data.length ; i++){
